a4d87f80765f16bf1189cf8ad0875da2641f9995
Nathan Laxague <anotherwavefromnathan@protonmail.com>
2026-07-22 11:30:46 -0400

    Remove wind-wave schematic and renumber figures. Removal per referee comments.

--- aa_step01_figure_generation_script.m
+++ aa_step01_figure_generation_script.m
@@ -71,29 +71,9 @@ for fignum = fig_list
 
     switch fignum
 
-        %% Figure 01 - cartoon of wind-wave interaction
+        %% Figure 01 - map of Cape Cod, zoom into ASIT location
 
         case 1
-
-            figure(fignum);clf
-            set(fignum,'Position',full_pos.*[1 1 0.5 0.5])
-
-            wind_wave_schematic_topdown(fignum,fsize)
-
-            if print_flag
-
-                figure(fignum)
-                pause(0.5)
-                print([fig_folder 'wind_wave_schematic_topdown.svg'],'-dsvg')
-                pause(0.5)
-                print([fig_folder 'wind_wave_schematic_topdown.png'],'-dpng',raster_dpi_string)
-
-            end
-
-
-            %% Figure 02 - map of Cape Cod, zoom into ASIT location
-
-        case 2
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 1 0.55])
@@ -111,9 +91,9 @@ for fignum = fig_list
             end
 
 
-            %% Figure 03 - windrose (combine with ASIT schematic)
+            %% Figure 02 - windrose (combine with ASIT schematic)
 
-        case 3
+        case 2
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 0.5 0.5])
@@ -129,9 +109,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 04 - stacked timeseries/histograms of key variables
+            %% Figure 03 - stacked timeseries/histograms of key variables
 
-        case 4
+        case 3
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 1 1])
@@ -148,9 +128,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 05 - directional spectrum and direction-integrated outputs
+            %% Figure 04 - directional spectrum and direction-integrated outputs
 
-        case 5
+        case 4
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 0.5 1])
@@ -167,9 +147,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 06 - frequency and wavenumber omnispect
+            %% Figure 05 - frequency and wavenumber omnispect
 
-        case 6
+        case 5
 
             figure(fignum);clf
             set(fignum,'Position',full_pos)
@@ -186,9 +166,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 07 - frequency and wavenumber spectral subranges
+            %% Figure 06 - frequency and wavenumber spectral subranges
 
-        case 7
+        case 6
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 0.5 1.33])
@@ -205,9 +185,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 08 - scaled wavenumber spectra and transition wavenumbers
+            %% Figure 07 - scaled wavenumber spectra and transition wavenumbers
 
-        case 8
+        case 7
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 0.5 1])
@@ -224,9 +204,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 09 - normalized transition wavenumber
+            %% Figure 08 - normalized transition wavenumber
 
-        case 9
+        case 8
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 1 0.55])
@@ -243,9 +223,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 10 - directional spectra comparison: f, k
+            %% Figure 09 - directional spectra comparison: f, k
 
-        case 10
+        case 9
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 1 0.75])
@@ -262,9 +242,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 11 - breaking crest length distribution and S_ds
+            %% Figure 10 - breaking crest length distribution and S_ds
 
-        case 11
+        case 10
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 0.5 1])
@@ -281,9 +261,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 12 - short wave spectrum with S_ds contours and wavenumber-integrated quantities (e.g., saturation-range spreading function and S_ds)
+            %% Figure 11 - short wave spectrum with S_ds contours and wavenumber-integrated quantities (e.g., saturation-range spreading function and S_ds)
 
-        case 12
+        case 11
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 0.5 1.33])
@@ -300,9 +280,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 13 - S_ds(theta), binned by wave age
+            %% Figure 12 - S_ds(theta), binned by wave age
 
-        case 13
+        case 12
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 0.5 1])
@@ -319,9 +299,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 14 - wave breaking momentum flux and breaking direction
+            %% Figure 13 - wave breaking momentum flux and breaking direction
 
-        case 14
+        case 13
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 0.5 1])
@@ -338,12 +318,12 @@ for fignum = fig_list
 
             end
 
-            %% Figure 15 - wave-relative wind direction with wind speed
+            %% Figure 14 - wave-relative wind direction with wind speed
 
-        case 15
+        case 14
 
             figure(fignum);clf
-            set(fignum,'Position',full_pos.*[1 1 1 0.95])
+            set(fignum,'Position',full_pos.*[1 1 1 1.25])
 
             wind_wave_subrange_directions(fignum,fsize)
 
@@ -357,9 +337,9 @@ for fignum = fig_list
 
             end
 
-            %% Figure 16-17 - directional spreading, binned by wave age and plotted against normalized wavenumber
+            %% Figure 15-16 - directional spreading, binned by wave age and plotted against normalized wavenumber
 
-        case 16
+        case 15
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 1 1])
@@ -378,7 +358,7 @@ for fignum = fig_list
 
             end
 
-        case 17
+        case 16
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 1 1])
@@ -397,7 +377,7 @@ for fignum = fig_list
 
             %% Figure A1
 
-        case 18
+        case 17
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 1 1 0.5])
@@ -416,7 +396,7 @@ for fignum = fig_list
 
             %% Figure B1
 
-        case 19
+        case 18
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 0.5 0.5 1.25])
@@ -435,7 +415,7 @@ for fignum = fig_list
 
             %% Figure C1
 
-        case 20
+        case 19
 
             figure(fignum);clf
             set(fignum,'Position',full_pos.*[1 0.5 0.5 1])
